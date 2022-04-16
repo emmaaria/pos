6cff6b53344b39fcb1a9bdf7b80ce56da6bbc798
shakilapervin <emma@else.co.il>
2022-04-17 00:02:11 +0600

    Delete product feature added

--- pages/api/product/delete.js
+++ pages/api/product/delete.js
@@ -1,14 +1,14 @@
 import db from "../../../lib/db";
 import session from "../../../lib/session";
 import {withIronSessionApiRoute} from 'iron-session/next';
-import CustomerModel from "../../../models/Customer";
+import ProductModel from "../../../models/Product";
 export default withIronSessionApiRoute(async (req, res) => {
     if (req.session.user){
         const id = req.body.id;
         await db.connect();
-        const customer = await CustomerModel.findByIdAndDelete(id);
+        const product = await ProductModel.findByIdAndDelete(id);
         
-        if (customer) {
+        if (product) {
             res.status(201).send({
                 success: 'Deleted',
             });
