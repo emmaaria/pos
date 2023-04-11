6e492abbd2d84bffecc674439941f5d254036e91
emmaaria <emma@else.co.il>
2023-04-12 00:28:17 +0600

    New

--- src/components/AutocompleteDefaultCustomer.js
+++ src/components/AutocompleteDefaultCustomer.js
@@ -8,6 +8,7 @@ export default function AutocompleteDefaultCustomer({name, id, token, className}
     };
     const [timer, setTimer] = useState(null);
     const [data, setData] = useState();
+    const [cid, setCid] = useState(id);
     const [keyword, setKeyword] = useState();
     const [searching, setSearching] = useState(false);
     const search = async (value) => {
@@ -37,6 +38,7 @@ export default function AutocompleteDefaultCustomer({name, id, token, className}
     const setValue = (label, value) => {
         $('.customer-input').val(label);
         $('.customer-id').val(value);
+        setCid(value)
         setKeyword(null)
     }
     return (
@@ -44,10 +46,11 @@ export default function AutocompleteDefaultCustomer({name, id, token, className}
             <div className={`autocompleteWrapper`}>
                 <input type="text" className={`form-control autocompleteInput customer-input ${className}`} autoComplete={`off`}
                        defaultValue={name}
-                       onKeyUp={search}
-                       onKeyDown={search}
-                       onChange={search}/>
-                <input type="hidden" className={`customer-id`} defaultValue={id}/>
+                       onKeyUp={(e) => search(e.target.value)}
+                       onKeyDown={(e) => search(e.target.value)}
+                       onChange={(e) => search(e.target.value)}
+                />
+                <input type="hidden" className={`customer-id`} value={cid}/>
                 {
                     keyword && (
                         <div className={`autocompleteItemContainer customer`}>
